914c988b505e8370d097f0fdcb6a7105af1741e9
sdoerm <sdoerm@berkeley.edu>
2017-07-09 23:49:26 -0700

    Create allergen_button.rb

---
+++ features/step_definitions/allergen_button.rb
@@ -0,0 +1,34 @@
+
+Given /I am on the home page of the application/ do
+    pending
+end
+
+
+Then /I should see (.*) on the map/ do |item|
+    pending
+end
+
+
+Then /I should see (.*) on the bar/ do |item|
+    pending
+=begin
+        result = false
+        if item.include? "Recent Searches"
+            result = ...
+        elsif item.include? "View Favorites"
+            result = ...
+        elsif item.include? "allergen button"
+            result = ...
+        end       
+        assert result
+=end
+end
+
+Then /I should be able to add an allergen to the map/ do
+    pending
+end
+
+
+When /I click the add allergen button/ do
+    pending
+end
