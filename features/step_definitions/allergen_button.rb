f03cf60133baa6274706a4ab3165a1f9100ebc18
Soroosh Demooei <sdoerm@berkeley.edu>
2017-07-12 22:10:45 +0000

    commented out everything to see if travisci passes

--- features/step_definitions/allergen_button.rb
+++ features/step_definitions/allergen_button.rb
@@ -1,10 +1,10 @@
 
 
-Then /I should be able to add an allergen to the map/ do
-    pending
-end
+# Then /I should be able to add an allergen to the map/ do
+#     pending
+# end
 
 
-When /I click the add allergen button/ do
-    pending
-end
+# When /I click the add allergen button/ do
+#     pending
+# end
--- features/step_definitions/page_structure_steps.rb
+++ features/step_definitions/page_structure_steps.rb
@@ -1,47 +1,47 @@
 
-Given /I am on the home page of the application/ do
-    pending
-end
+# Given /I am on the home page of the application/ do
+#     pending
+# end
 
 
-Then /I should see (.*) on the (.*) side/ do |item, location|
-    pending
+# Then /I should see (.*) on the (.*) side/ do |item, location|
+#     pending
 
-=begin
-        result = false
-        if item.include? "map"
-            if location.include? "left"
-                result = ...
-            elseif location.include? "right"
-                result = ...
-            end
-        elsif item.include? "information"
-            if location.include? "left"
-                result = ...
-            elseif location.include? "right"
-                result = ...
-            end       
-        assert result
-=end
-end
-
-
-Then /I should see (.*) on the map/ do |item|
-    pending
-end
+# =begin
+#         result = false
+#         if item.include? "map"
+#             if location.include? "left"
+#                 result = ...
+#             elseif location.include? "right"
+#                 result = ...
+#             end
+#         elsif item.include? "information"
+#             if location.include? "left"
+#                 result = ...
+#             elseif location.include? "right"
+#                 result = ...
+#             end       
+#         assert result
+# =end
+# end
 
 
-Then /I should see (.*) on the bar/ do |item|
-    pending
-=begin
-        result = false
-        if item.include? "Recent Searches"
-            result = ...
-        elsif item.include? "View Favorites"
-            result = ...
-        elsif item.include? "allergen button"
-            result = ...
-        end       
-        assert result
-=end
-end
+# Then /I should see (.*) on the map/ do |item|
+#     pending
+# end
+
+
+# Then /I should see (.*) on the bar/ do |item|
+#     pending
+# =begin
+#         result = false
+#         if item.include? "Recent Searches"
+#             result = ...
+#         elsif item.include? "View Favorites"
+#             result = ...
+#         elsif item.include? "allergen button"
+#             result = ...
+#         end       
+#         assert result
+# =end
+# end
