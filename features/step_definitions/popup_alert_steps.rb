c02ca9f5bd269bc259c1a35e562a42ea14a37193
Soroosh Demooei <sdoerm@berkeley.edu>
2017-07-13 02:33:37 +0000

    uncommented all comments

--- features/step_definitions/popup_alert_steps.rb
+++ features/step_definitions/popup_alert_steps.rb
@@ -1,41 +1,41 @@
 
-# When /I am (not)? logged in/ do |logged_in|
-#   # Does nothing when not logged in
-#   # Otherwise, logs in as generic user
-#   pending
-# end
+When /I am (not)? logged in/ do |logged_in|
+  # Does nothing when not logged in
+  # Otherwise, logs in as generic user
+  pending
+end
   
-# And /(.*) is inputted to (.*) (.*) times/ do |allergen, city, times| 
-#   # Specifies what allergy is inputted to what city
-#   # however many number of times
+And /(.*) is inputted to (.*) (.*) times/ do |allergen, city, times| 
+  # Specifies what allergy is inputted to what city
+  # however many number of times
+  pending
+end
+
+# Then /I should (not)? see (.*)/ do |string|
+#   # Dictates whether something should be displayed
 #   pending
 # end
 
-# # Then /I should (not)? see (.*)/ do |string|
-# #   # Dictates whether something should be displayed
-# #   pending
-# # end
-
-# # When /my favorite (city is | cities are) (.*) (and (.*))?/ do |cities*|
-# #     #Determine which cities are favorite
-# #     pending
-# # end
-
-# And /I am allergic to (.*) (and (.*))?/ do |allergen1, allergens|
-# # For specified user, add specifies allergens to user
-# pending
+# When /my favorite (city is | cities are) (.*) (and (.*))?/ do |cities*|
+#     #Determine which cities are favorite
+#     pending
 # end
 
-# # When /I press (.*)/ do |button_name|
-# #  # Just click on the button
-# #  # Web steps takes care of this already, I think
-# #  pending
-# # end
+And /I am allergic to (.*) (and (.*))?/ do |allergen1, allergens|
+# For specified user, add specifies allergens to user
+pending
+end
 
-# # When /I uncheck (.*)/ do |form_name|
-# #     # Uncheck
-# #     # will use same code as on movie_steps
-# #   pending
-# # end
+# When /I press (.*)/ do |button_name|
+#  # Just click on the button
+#  # Web steps takes care of this already, I think
+#  pending
+# end
+
+# When /I uncheck (.*)/ do |form_name|
+#     # Uncheck
+#     # will use same code as on movie_steps
+#   pending
+# end
  
 
