25a80789035bd7e74dc7aa7a5b3e6729e3a804d6
Dominic Bueno <dominicbueno@berkeley.edu>
2017-07-15 21:49:18 +0000

    updated Google+ API key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, '819799125663-sd5t7f7a1i0ho6qk0p187d9usbs53i9t.apps.googleusercontent.com', 'lMHNvX_MEw33l83d_MyqKaj7',
+    provider :google_oauth2, '1055989244914-0p80h2cf6bbemstj59b9f1uf248la77h.apps.googleusercontent.com', 'J09H9sH9CMoFLqgyPov5-udi',
     {client_options: {ss1: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
