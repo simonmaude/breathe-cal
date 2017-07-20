e7f1612f354336a722747b961e17ae5751d4cbd9
Soroosh Demooei <sdoerm@berkeley.edu>
2017-07-20 02:17:52 +0000

    maint: added feature files for first_load

--- features/step_definitions/first_load_steps.rb
+++ features/step_definitions/first_load_steps.rb
@@ -1,7 +1,30 @@
-Then /my current location has to be inside the map/ do
+Then /"(.*)" should be inside the map/ do |city|
     pending
 end
 
-Then /the side bar city has to be near me/ do
+Then /the side bar city has to be near "(.*)"/ do |city|
+    pending
+end
+
+
+And /I searched city: (.*)/ do |city_name|
+    pending
+end
+
+And /"(.*)" should (not) be inside the map/ do
+    pending
+end
+
+
+And /my current location is "(.*)"/ do |current_location|
+    pending
+end
+
+
+Then(/^the side bar city should be near: "([^"]*)"$/) do |city|
+    pending
+end
+
+Then(/^the side bar city should not be near "([^"]*)"$/) do |city|
     pending
 end
