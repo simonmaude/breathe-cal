39a19abd66c97d5bd2969ddd810e213f27c6e49d
Angus Hung <angus.hung@berkeley.edu>
2017-07-20 06:30:23 +0000

    fix build issue 2

--- features/step_definitions/add_filter_steps.rb
+++ features/step_definitions/add_filter_steps.rb
@@ -1,7 +1,3 @@
 When /I add a filter with (.*) (and (.*))?/ do |allergen1, *allergens|
- pending
-end  
-
-Then /I should (not)? see a marker with (.*) at (.*)/ do |not_seen, allergen, location|
     pending
-end
+end  
