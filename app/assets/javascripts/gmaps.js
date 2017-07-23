cc9c12d968b20aeaaeb8025117ad965c9b1d64d0
Simon <simonmaude@gmail.com>
2017-07-23 01:25:44 +0000

    changed global_markers function to only pick one marker per user, marker functions now work on allergen title or checkboxes, updated rspec tests

--- app/assets/javascripts/gmaps.js
+++ app/assets/javascripts/gmaps.js
@@ -13,8 +13,6 @@ var heatmapData = [];
 
 function initAutocomplete() {
   
-  var labelNum = 0;
-  
   function point2LatLng(point, map) {
     var topRight = map.getProjection().fromLatLngToPoint(map.getBounds().getNorthEast());
     var bottomLeft = map.getProjection().fromLatLngToPoint(map.getBounds().getSouthWest());
@@ -25,7 +23,6 @@ function initAutocomplete() {
   
   function fetchMarkers(){
     deleteMarkers();
-    labelNum = 0;
     var bounds = map.getBounds();
     var NECorner = bounds.getNorthEast();
     var SWCorner = bounds.getSouthWest();
@@ -298,9 +295,7 @@ function initAutocomplete() {
   }
   
   function placeMarker(location) {
-    labelNum += 1;
     var marker = new google.maps.Marker({
-      label: labelNum.toString() ,
       position: location,
       map: map,
       draggable: true,
@@ -340,7 +335,6 @@ function initAutocomplete() {
     recentMarker = marker;
     
     var listenerHandle = google.maps.event.addListener(infowindow, 'closeclick', function(){
-      labelNum -=1;
       recentMarker.setMap(null);
       recentMarker = null;
     });
--- app/models/marker.rb
+++ app/models/marker.rb
@@ -4,12 +4,12 @@ class Marker < ActiveRecord::Base
   @@allergen_list = [:dog, :cat, :mold, :bees, :perfume, :oak, :dust, :smoke, :gluten, :peanut]
   
   
-  def self.find_all_in_bounds(top, bottom, left, right, id = '')
-    markersTop = Marker.where("lat < (?)", top).where(id)
+  def self.find_all_in_bounds(top, bottom, left, right, id = '', search_allergen = '')
+    markersTop = Marker.where("lat < (?)", top).where(id).where(search_allergen)
     markersBottom = Marker.where("lat > (?)", bottom)
     markersLeft = Marker.where("lng < ?", left)
     markersRight = Marker.where("lng > ?", right)
-    return  markersBottom & markersTop & markersRight & markersLeft
+    return markersBottom & markersTop & markersRight & markersLeft
   end
  
   def self.find_all_in_zoom(top,bottom,left,right,lat, long)        
@@ -24,7 +24,7 @@ class Marker < ActiveRecord::Base
   end  
       
       
-  def self.get_global_markers(markers,global_number_show,top,bottom,left,right)
+  def self.get_global_markers(markers,global_number_show,top,bottom,left,right,search_allergen = '')
     output = []
     # for each marker that is not listed already
     markers.each do |marker|
@@ -34,20 +34,25 @@ class Marker < ActiveRecord::Base
         # check all other markers in zoomed in area to see if global_show - 1 are also true
         zoom_markers = self.find_all_in_zoom(top.to_f,bottom.to_f,left.to_f,right.to_f,marker.lat.to_f, marker.lng.to_f)
         # for every allergen
-        @@allergen_list.each do |allergen|
-          # breakout of loop if marker has been added already
+        search_allergen == '' ? (allergens = @@allergen_list) : (allergens = [search_allergen])
+        allergens.each do |allergen|
+          # breakout of loop if marker has multiple allergens and has been added already
           if break_test == true then break end
           allergen_count = 0
-          # if the marker has this allergen ticked
-          if (marker.send(allergen) == true) 
+          # if the marker has this allergen checkbox ticked or title = allergen 
+          if (marker.send(allergen) == true) || marker.title == allergen
+            id_set = Set.new
             # for every marker in the zoomed area around this marker
             zoom_markers.each do |zoom_marker|
               # if it also has this allergen ticked
-              if (zoom_marker.send(allergen) == true) 
+              if (zoom_marker.send(allergen) == true) || zoom_marker.title == allergen
+                # if the allergen_count >= global_number_show and this user does not 
+                # already have a marker for this allergen saved 
                 allergen_count += 1
-                if allergen_count >= global_number_show
-                  # add marker to ouput if > global appear in zoomed area 
+                if (allergen_count >= global_number_show) && (!id_set.include? marker.client_id)
+                  # add marker to ouput and the client id to the user id set  
                   output << marker
+                  id_set << marker.client_id
                   # breakout of loop if marker has been added already
                   break_test = true
                   break
