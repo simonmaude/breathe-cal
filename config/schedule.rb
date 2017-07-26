bdf680c344b499b5b1623c3e7a5ce308f464b4c4
Albert Tung <albertytung@berkeley.edu>
2017-07-26 05:13:15 +0000

    feat: Begin working on daily email branch

---
+++ config/schedule.rb
@@ -0,0 +1,23 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+# Task to run daily emails
+every 1.day, :at => '6:00 am' do
+  #runner "Client.daily_email"
+end
+
