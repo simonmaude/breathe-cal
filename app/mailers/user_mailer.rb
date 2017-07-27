5b54a05a2e84c1bc749d822ea31878ce2840b451
Albert Tung <albertytung@berkeley.edu>
2017-07-27 06:18:44 +0000

    feat: Added email templates, updates for alert and daily digest

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,6 +6,7 @@ class UserMailer < ApplicationMailer
       mail(to: @user.email, subject: 'Sample email')
   end
   
+  # Used for testing mailgun API
   def mailgunner(user)
     @user = user
     mg_client = Mailgun::Client.new(ENV[MAILGUN_KEY])
@@ -14,5 +15,15 @@ class UserMailer < ApplicationMailer
                       :subject => 'Sample Mail using Mailgun API',
                       :text    => 'This mail is sent using Mailgun API via mailgun-ruby'}
     mg_client.send_message("sandbox0880c15a47cd403ea19793a12bd76d5f.mailgun.org", message_params)
+  end
+  
+  def email_alert(user, allergen_status)
+    @user = user
+    mail to: @user.email, subject: "Emergency Air Alert"
+  end
+  
+  def daily_digest(user)
+    @user = user
+    mail to: @user.email, subject: "Daily  Quality Digest"
   end
 end
