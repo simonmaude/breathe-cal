8ead6c2d10cfc6bf0b1ab170ff6a5a8ab9c95360
Soroosh Demooei <sdoerm@berkeley.edu>
2017-07-27 02:59:47 +0000

    maint: added feature files for translation

---
+++ features/step_definitions/translate_steps.rb
@@ -0,0 +1,32 @@
+
+And /I click on settings/ do
+    pending
+end
+
+Then /I should see a tab for language/ do
+    pending
+end
+
+And /I click on the language tab/ do
+    pending
+end
+
+
+Then /I should be able to choose "(.*)"/ do |language|
+    pending
+end
+
+
+And /I select the language "(.*)"/ do |language|
+    pending
+end
+
+
+Then /I should see "(.*)"/ do |word|
+    pending
+end
+
+
+Then /I should not see "(.*)"/ do |word|
+    pending
+end
