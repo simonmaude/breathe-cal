795e593d08c1f478f9dbfd9c1b7fd60d8fb928f2
Albert Tung <albertytung@berkeley.edu>
2017-07-30 23:25:10 +0000

    impr: Made breathe california logo appear on daily digest email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,19 @@
 require 'mailgun'
-class UserMailer < ApplicationMailer
+class UserMailer < ActionMailer::Base 
   default from: ENV['MAILGUN_USER_NAME']
   def signup_email(user)
       @user = user
       mail(to: @user.email, subject: 'Sample email')
   end
   
+  def email_image_tag(image, **options)
+    attachments[image] = {
+        :data => File.read(Rails.root.join("app/assets/images/#{image}")),
+        :mime_type => "image/#{image.split('.').last}",
+        #:encoding => "base64"
+        }
+        return attachments[image]
+    end
   # Used for testing mailgun API
   def mailgunner(user)
     @user = user
@@ -21,12 +29,42 @@ class UserMailer < ApplicationMailer
   def email_alert(user, alert)
     @user = user
     @alert = alert 
+    
+    attachments.inline["bcba_logo.png"] = 
+      #File.read("#{Rails.root}/app/assets/images/bcba_logo.png")
+      File.read(File.join(Rails.root, "app", "assets", "images", "bcba_logo.png"))
+      
+      
+    attachments.inline["email_borderup.png"] = 
+      File.read("#{Rails.root}/app/assets/images/email_borderup.png")
+      
+    attachments.inline["email_borderdown.png"] = 
+      File.read("#{Rails.root}/app/assets/images/email_borderdown.png")
+      
+    
     mail to: @user.email, subject: "Emergency Air Alert"
   end
   
   def daily_digest(user, alert)
     @user = user
     @alert = alert
+    attachments.inline["bcba_logo.png"] = File.read("#{Rails.root}/app/assets/images/bcba_logo.png")
+    #attachments.inline["bcba_logo.png"] =  #File.read(Rails.root.join("app", "assets", "images", "bcba_logo.png"))
+    #{
+    #  :data => File.read(Rails.root.join("app", "assets", "images", "bcba_logo.png")),
+    #  :mime_type => "image/png",
+    #  :encoding => "base64"
+    #}
+    #  File.read("#{Rails.root}/app/assets/images/bcba_logo.png")
+      
+      
+    #attachments.inline["email_borderup.png"] = 
+    #  File.read("#{Rails.root}/app/assets/images/email_borderup.png")
+      
+    #attachments.inline["email_borderdown.png"] = 
+    #  File.read("#{Rails.root}/app/assets/images/email_borderdown.png")
+    
+    
     mail to: @user.email, subject: "Daily  Quality Digest"
   end
 end
