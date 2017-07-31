d29e6c0108157d397525d3214ad2e429c5a6ef47
Simon <simonmaude@gmail.com>
2017-07-31 20:22:33 +0000

    fix:  added speed ups to marker sql queries

--- spec/models/marker_spec.rb
+++ spec/models/marker_spec.rb
@@ -32,31 +32,31 @@ RSpec.describe Marker, type: :model do
   describe ".get_global_markers" do
     it "returns no markers if less than the global_number_show are present on the map" do
       global_number_show = 5
-      Marker.create!(lat: 15, lng: 15, title: "dog")
-      Marker.create!(lat: 14, lng: 14, title: "dog")
-      Marker.create!(lat: 30, lng: 30, title: "dog")
+      Marker.create!(lat: 15, lng: 15, client_id: 1, title: "dog")
+      Marker.create!(lat: 14, lng: 14, client_id: 2, title: "dog")
+      Marker.create!(lat: 30, lng: 30, client_id: 3, title: "dog")
       markers = Marker.find_all_in_bounds({top:20, bottom:10, left:20, right:10})
       expect(Marker.get_global_markers(markers,global_number_show,{top:20, bottom:10, left:20, right:10}).length).to eq(0)
     end
     
     it "returns no markers if less than the global_number_show are present in a zoomed area" do
       global_number_show = 5
-      Marker.create!(lat: 15, lng: 15, title: "dog")
-      Marker.create!(lat: 15, lng: 15, title: "dog")
-      Marker.create!(lat: 15, lng: 15, title: "dog")
-      Marker.create!(lat: 15, lng: 15, title: "dog")
-      Marker.create!(lat: 20, lng: 20, title: "dog")
+      Marker.create!(lat: 15, lng: 15, client_id: 1, title: "dog")
+      Marker.create!(lat: 15, lng: 15, client_id: 2, title: "dog")
+      Marker.create!(lat: 15, lng: 15, client_id: 3, title: "dog")
+      Marker.create!(lat: 15, lng: 15, client_id: 4, title: "dog")
+      Marker.create!(lat: 20, lng: 20, client_id: 5,title: "dog")
       markers = Marker.find_all_in_bounds({top:20, bottom:10, left:20, right:10})
       expect(Marker.get_global_markers(markers,global_number_show,{top:20, bottom:10, left:20, right:10}).length).to eq(0)
     end
     
     it "returns no markers if less than the global_number_show of one type are present in a zoomed area" do
       global_number_show = 5
-      Marker.create!(lat: 15, lng: 15, title: "dog")
-      Marker.create!(lat: 15, lng: 15, title: "dog")
-      Marker.create!(lat: 15, lng: 15, title: "dog")
-      Marker.create!(lat: 15, lng: 15, title: "dog")
-      Marker.create!(lat: 15, lng: 15, title: "cat")
+      Marker.create!(lat: 15, lng: 15, client_id: 1, title: "dog")
+      Marker.create!(lat: 15, lng: 15, client_id: 2, title: "dog")
+      Marker.create!(lat: 15, lng: 15, client_id: 3, title: "dog")
+      Marker.create!(lat: 15, lng: 15, client_id: 4, title: "dog")
+      Marker.create!(lat: 15, lng: 15, client_id: 5, title: "cat")
       markers = Marker.find_all_in_bounds({top:20, bottom:10, left:20, right:10})
       expect(Marker.get_global_markers(markers,global_number_show,{top:20, bottom:10, left:20, right:10}).length).to eq(0)
     end
@@ -82,6 +82,18 @@ RSpec.describe Marker, type: :model do
       markers = Marker.find_all_in_bounds({top:20, bottom:10, left:20, right:10})
       expect(markers.length).to eq(5)
       expect(Marker.get_global_markers(markers,global_number_show,{top:20, bottom:10, left:20, right:10}).length).to eq(4)
+    end    
+    
+    it "returns markers if greater than the global_number_show are present in a zoomed area" do
+      global_number_show = 4
+      Marker.create!(lat: 15, lng: 15, client_id: 1, title: "dog")
+      Marker.create!(lat: 15, lng: 15, client_id: 2, title: "dog")
+      Marker.create!(lat: 15, lng: 15, client_id: 3, title: "dog")
+      Marker.create!(lat: 15, lng: 15, client_id: 4, title: "dog")
+      Marker.create!(lat: 15, lng: 15, client_id: 5, title: "dog")
+      markers = Marker.find_all_in_bounds({top:20, bottom:10, left:20, right:10})
+      expect(markers.length).to eq(5)
+      expect(Marker.get_global_markers(markers,global_number_show,{top:20, bottom:10, left:20, right:10}).length).to eq(5)
     end
   end
 
