e0af46c81b3c90a10e48eccdebc1cb71ee888695
Soroosh Demooei <sdoerm@berkeley.edu>
2017-08-05 10:17:23 +0000

    fix: functional

--- db/migrate/20170805091252_add_email_security.rb
+++ db/migrate/20170805091252_add_email_security.rb
@@ -3,7 +3,8 @@ class AddEmailSecurity < ActiveRecord::Migration
     change_table :clients do |t|
       t.string :email_key
       t.string :language
-      t.string :email_is_confirmed
+      t.boolean :email_is_confirmed
+      t.datetime :key_creation_time
     end
   end
 end
