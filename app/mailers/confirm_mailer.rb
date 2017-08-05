08d6fe33e9248c6cc246e0cbf24c880db90f8ca9
Soroosh Demooei <sdoerm@berkeley.edu>
2017-08-05 21:58:50 +0000

    fix: her

--- app/mailers/confirm_mailer.rb
+++ app/mailers/confirm_mailer.rb
@@ -2,10 +2,11 @@ class ConfirmMailer < ApplicationMailer
 
   
   def confirm_email(name, user_id, user_email, random_number)
+    @root_email = "https://tranquil-wildwood-40360.herokuapp.com/"
     @user_email = user_email
     @user_name = name
-    @confirm_link = "https://work-sordem.c9users.io/email_confirm/" + user_id + "/" + random_number
-    @delete_link = "https://work-sordem.c9users.io/delete_email/" + user_id + "/" + random_number
+    @confirm_link = @root_email + user_id + "/" + random_number
+    @delete_link = @root_email + user_id + "/" + random_number
     mail to: user_email, subject: "Please Confirm Your Email", template_name: 'confirm_email.html.erb'
   end
   
