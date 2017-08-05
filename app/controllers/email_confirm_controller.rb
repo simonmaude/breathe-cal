821118191fd34d6d237ae13e56b84c1128eea9e1
Soroosh Demooei <sdoerm@berkeley.edu>
2017-08-05 06:25:18 +0000

    fix: fixing pages

--- app/controllers/email_confirm_controller.rb
+++ app/controllers/email_confirm_controller.rb
@@ -1,8 +1,11 @@
 class EmailConfirmController < ApplicationController
     
     def confirming
-        render 'confirm_mailer/confirmed.html'
-        # render 'confirm_mailer/confirm_failed.html'
+        if (params[:num] == "2")
+            render 'confirm_mailer/confirmed.html'
+        else
+            render 'confirm_mailer/confirm_failed.html'
+        end
     end
     
 end
