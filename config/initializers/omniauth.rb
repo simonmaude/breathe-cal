454858053f51fd199b3c55d3a8b6d950fba2cda8
Dominic Bueno <dominicbueno@berkeley.edu>
2017-08-06 03:35:34 +0000

    combined simon and dominic sidebar

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :google_oauth2, '1055989244914-0p80h2cf6bbemstj59b9f1uf248la77h.apps.googleusercontent.com', 'J09H9sH9CMoFLqgyPov5-udi',
-    # provider :google_oauth2, '956271863410-gor5funl3ro57pkkbntm1jhepsqbspqj.apps.googleusercontent.com', 'KGhajDb7f6pmyS_9OuNevcGg',
+   #provider :google_oauth2, '956271863410-gor5funl3ro57pkkbntm1jhepsqbspqj.apps.googleusercontent.com', 'KGhajDb7f6pmyS_9OuNevcGg',
     {client_options: {ss1: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
