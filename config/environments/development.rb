b046a8ebb8839c18b60263429ee976ffac2d24d1
Albert Tung <albertytung@berkeley.edu>
2017-08-10 19:58:28 +0000

    fix:  Re-added changes to EmailManager for class methods (from feature-mailer)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,4 +50,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.after_initialize do
+    EmailManager.update_alert_status
+  end
+  
 end
