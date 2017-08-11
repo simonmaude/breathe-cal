7e19bea879ef2603e1ad535b0abe39e8d4a814f6
Soroosh Demooei <sdoerm@berkeley.edu>
2017-08-11 06:30:29 +0000

    mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  before_filter :prepare_for_mobile
-  
+
   helper_method :current_client
 
   
@@ -13,19 +12,6 @@ class ApplicationController < ActionController::Base
   end
 
   private
-  
-  def mobile_device?
-    if session[:mobile_param]
-      session[:mobile_param] == "1"
-    else
-      request.user_agent =~ /Mobile|webOS/
-    end
-  end
-  helper_method :mobile_device?
 
-  def prepare_for_mobile
-    session[:mobile_param] = params[:mobile] if params[:mobile]
-    request.format = :mobile if mobile_device? && !request.xhr?
-  end
   
 end
